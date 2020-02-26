9df7f0b01a89fab4920806d0f1b93f1491def4f2
Jeff Paril <55180317+jeffersonfparil@users.noreply.github.com>
2020-02-26 20:02:11 +1100

    Dev (#8)
    updating tests

--- tests/test2.R
+++ tests/test2.R
@@ -1,4 +1,4 @@
 library(violinplotter)
 str(dummy_data)
-OUT_1 = violinplotter(formula = RESPONSE_1 ~ STRATUM*TREATMENT, data=dummy_data, REGRESSX=c(TRUE, FALSE, FALSE))
+OUT_1 = violinplotter(formula = RESPONSE_1 ~ STRATUM*TREATMENT, data=dummy_data, XCATEGOR=c(FALSE,TRUE,TRUE), REGRESSX=c(TRUE, FALSE, FALSE), HSD=c(TRUE, TRUE, FALSE))
 OUT_2 = violinplotter(formula = RESPONSE_2 ~ STRATUM*TREATMENT, data=dummy_data)
