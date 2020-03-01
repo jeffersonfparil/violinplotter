f3651c165334c367c14e746c91a6dcdcb09d37d1
Jeff Paril <55180317+jeffersonfparil@users.noreply.github.com>
2020-03-01 18:46:18 +1100

    Dev (#11)
    corrections based on cran requirements and best practices

--- R/mean_comparison_HSD.R
+++ R/mean_comparison_HSD.R
@@ -38,11 +38,11 @@ mean_comparison_HSD = function(formula, data=NULL, explanatory_variable_name, al
   mod = aov(formula, data=df)
   anova_table = as.data.frame(anova(mod))
   if (anova_table$Pr[rownames(anova_table) == explanatory_variable_name] < alpha){
-    print(anova_table)
-    print(paste0(explanatory_variable_name, " has a significant effect on the response variable!"))
+    message(anova_table)
+    message(paste0(explanatory_variable_name, " has a significant effect on the response variable!"))
   } else {
-    print(anova_table)
-    print(paste0(explanatory_variable_name, " has a no significant effect on the response variable!"))
+    message(anova_table)
+    message(paste0(explanatory_variable_name, " has a no significant effect on the response variable!"))
   }
   ### computate the means per explanatory variable level
   means = eval(parse(text=paste0("aggregate(",  response_var_name, "~ `", explanatory_variable_name, "`, data=df, FUN=mean)")))
