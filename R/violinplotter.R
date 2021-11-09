322ad15137442ee1c96ce754aabade55b93313e9
jeff <jeffersonparil@gmail.com>
2021-11-10 09:30:52 +1100

    will need to refactor everything! Fixing show means when HSDX==FALSE

--- R/violinplotter.R
+++ R/violinplotter.R
@@ -121,7 +121,7 @@ violinplotter = function(formula, data=NULL, TITLE="", XLAB="", YLAB="", VIOLIN_
     ERROR_BAR_COLOURS[3] = orig_ERROR_BAR_COLOURS[3]
   } 
 
-  ### Do we have to perform Tukey's hones significant difference test across the explanatory variable/s?
+  ### Do we have to perform Tukey's honest significant difference test across the explanatory variable/s?
   if (length(HSDX) != ncol(df)-1) {
     HSDX = rep(HSDX, times=ncol(df)-1)
   }
@@ -158,7 +158,7 @@ violinplotter = function(formula, data=NULL, TITLE="", XLAB="", YLAB="", VIOLIN_
                                           XTICKS=XCATEGOR[i],
                                           LOG=LOGX[i],
                                           BASE=LOGX_BASE[i])
-    if (HSDX[i]==TRUE){
+    if (HSDX[i]==TRUE) | (SHOW_MEANS==TRUE){
       message("======================================================")
       message(paste0("HSD Grouping: ", explanatory_var_names[i]))
       message("======================================================")
@@ -168,7 +168,7 @@ violinplotter = function(formula, data=NULL, TITLE="", XLAB="", YLAB="", VIOLIN_
                                     alpha=ALPHA,
                                     LOG=LOGX[i],
                                     BASE=LOGX_BASE[i],
-                                    PLOT=TRUE,
+                                    PLOT=HSDX[i],
                                     SHOW_SAMPLE_SIZE=SHOW_SAMPLE_SIZE,
                                     SHOW_MEANS=SHOW_MEANS)
     } else {HSD_out = NULL}
